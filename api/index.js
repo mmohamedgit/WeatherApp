b5ef0acede82eca0861375757440a734a12bf09d
MohamedM <mohamed.mohamed91@hotmail.com>
2022-12-16 02:19:32 -0500

    added changes

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
 app.get("/api", (req, res) => {
   res.setHeader("Content-Type", "text/html");
   res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-  res.sendFile(path.join(__dirname, "/client/build/index.html"));
+  res.sendFile(path.join(__dirname, "/client/index.html"));
 });
 
 app.post("/selectedCity", (req, res, next) => {
